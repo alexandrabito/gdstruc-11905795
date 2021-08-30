7fa0f1e7f3f4a4cbf0a1224d821e5ce9d0937186
Alexandra Carice Bito <alexandracarice.bito@benilde.edu.ph>
2021-08-30 17:19:33 +0800

    Added Sample Project

---
+++ Sample Project/src/com/alexandrabito/Main.java
@@ -0,0 +1,19 @@
+package com.alexandrabito;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //prints out Hello World!
+        //System.out.println("Hello World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+    }
+}
